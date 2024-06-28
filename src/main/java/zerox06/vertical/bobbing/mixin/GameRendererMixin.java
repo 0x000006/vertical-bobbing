64acdfffb7d65341f68abb43d2faee91360a0977
0x06 <56885523+0x000006@users.noreply.github.com>
2024-06-28 22:29:17 +0200

    1.14

--- src/main/java/zerox06/vertical/bobbing/mixin/GameRendererMixin.java
+++ src/main/java/zerox06/vertical/bobbing/mixin/GameRendererMixin.java
@@ -1,10 +1,9 @@
 package zerox06.vertical.bobbing.mixin;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.RotationAxis;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,12 +14,12 @@ import zerox06.vertical.bobbing.imixin.IPlayerEntityExtension;
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
 
-    @Inject(method = "bobView", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void bobView(MatrixStack matrices, float tickDelta, CallbackInfo ci, PlayerEntity playerEntity, float f, float g, float h) {
+    @Inject(method = "bobView", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/GlStateManager;rotatef(FFFF)V", ordinal = 1), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void bobView(float tickDelta, CallbackInfo ci, PlayerEntity playerEntity, float f, float g, float h) {
         IPlayerEntityExtension playerExtension = (IPlayerEntityExtension) playerEntity;
         float i = MathHelper.lerp(tickDelta, playerExtension.getLastVerticalSpeed(), playerExtension.getVerticalSpeed());
         i = (i * 0.5f) - i;
-        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(i * 5f));
+        GlStateManager.rotatef(i * 5f, 1.0f, 0.0f, 0.0f);
     }
 
 }
