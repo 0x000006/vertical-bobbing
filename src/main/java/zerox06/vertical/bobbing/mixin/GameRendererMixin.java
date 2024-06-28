8e6fd18812c3867f7b93d3455957e1fe82f93d64
0x06 <56885523+0x000006@users.noreply.github.com>
2024-06-28 22:27:50 +0200

    1.15

--- src/main/java/zerox06/vertical/bobbing/mixin/GameRendererMixin.java
+++ src/main/java/zerox06/vertical/bobbing/mixin/GameRendererMixin.java
@@ -2,9 +2,9 @@ package zerox06.vertical.bobbing.mixin;
 
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.RotationAxis;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,7 +20,7 @@ public class GameRendererMixin {
         IPlayerEntityExtension playerExtension = (IPlayerEntityExtension) playerEntity;
         float i = MathHelper.lerp(tickDelta, playerExtension.getLastVerticalSpeed(), playerExtension.getVerticalSpeed());
         i = (i * 0.5f) - i;
-        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(i * 5f));
+        matrices.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(i * 5f));
     }
 
 }
