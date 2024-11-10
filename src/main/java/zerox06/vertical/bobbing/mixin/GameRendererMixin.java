b16d371a0d19433b75678193ce7775e99c27d947
0x06 <56885523+0x000006@users.noreply.github.com>
2024-11-10 09:52:13 +0100

    v1.3.2

--- src/main/java/zerox06/vertical/bobbing/mixin/GameRendererMixin.java
+++ src/main/java/zerox06/vertical/bobbing/mixin/GameRendererMixin.java
@@ -1,9 +1,8 @@
 package zerox06.vertical.bobbing.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RotationAxis;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,9 +14,14 @@ import zerox06.vertical.bobbing.imixin.LivingEntityExtension;
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
 
-    @Inject(method = "bobView", at = @At(value = "TAIL", shift = At.Shift.BEFORE))
-    private void postBobView(MatrixStack matrices, float tickDelta, CallbackInfo ci, @Local PlayerEntity player) {
-        LivingEntityExtension lee = (LivingEntityExtension) player;
+    @Inject(method = "bobView", at = @At(value = "TAIL"))
+    private void postBobView(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+        LivingEntityExtension lee = (LivingEntityExtension) MinecraftClient.getInstance().getCameraEntity();
+
+        if (lee == null) {
+            return;
+        }
+
         float rot = MathHelper.lerp(tickDelta, lee.vB$getPrevRot(), lee.vB$getRot());
         matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(rot));
     }
